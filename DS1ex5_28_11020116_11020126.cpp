cee242b6a48bdc6775efd0aa35011985bb6e2e74
chhiun <148851605+chhiun@users.noreply.github.com>
2023-12-24 20:02:14 +0800

    Update DS1ex5_28_11020116_11020126.cpp

--- DS1ex5_28_11020116_11020126.cpp
+++ DS1ex5_28_11020116_11020126.cpp
@@ -1,14 +1,12 @@
-// ?餉?銝?11020116 瞏?蝢? ?餉?銝?11020126 ?剔??
-// 雿輻C++11
+// 電資三 11020116 潘皓群  電資三 11020126 鄭祐昀 
+// 有使用C++11
 
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <vector>
 #include <string>
-#include <cstring>
-#include <algorithm>
-
+#include <algorithm> 
 using namespace std;
 
 void GetCommand(int &command)
@@ -41,21 +39,29 @@ void GetCommand(int &command)
 void Inputtxt(ifstream &inputFile, string &filenum, bool &check)
 {
     string filename;
-    cout << "\nInput a file number: ";
-    cin >> filename;
-    filenum = filename;
-    filename = "input" + filename + ".txt";
-    inputFile.open(filename);
-    if (!inputFile) {
-        cout << "\n###" << filename << " does not exist! ###" << endl;
-        cin.clear();
-        check = false;
-    } else {
-        check = true;
+    while (filename != "0") {
+        cout << "\nInput a file number [0: quit]: ";
+        cin >> filename;
+        if (filename == "0") {
+            break;
+        }
+        filenum = filename;
+        filename = "input" + filename + ".txt";
+        inputFile.open(filename);
+        if (!inputFile) {
+            cout << "\n###" << filename << " does not exist! ###" << endl;
+            cin.clear();
+            check = false;
+        } else {
+            check = true;
+            break;
+        }
+        inputFile.close();
     }
 }
 
-class Pokemon {
+// 用來存pokemon的各項資訊 
+class Pokemon {  
 public:
     int number;
     std::string name;
@@ -73,7 +79,7 @@ public:
 
     Pokemon() : number(0), total(0), hp(0), attack(0), defense(0), 
                 spAtk(0), spDef(0), speed(0), generation(0), legendary(false) {}
-
+	//用來從txt中讀取pokemon資訊 
     static Pokemon Parse(const std::string &line) {
         std::istringstream iss(line);
         Pokemon p;
@@ -95,7 +101,7 @@ public:
         return p;
     }
 };
-
+//二元樹結構 
 class BSTNode {
 public:
     Pokemon data;
@@ -142,6 +148,14 @@ private:
         return node;
     }
 
+    void ClearNode(BSTNode *node) {
+        if (node) {
+            ClearNode(node->left);
+            ClearNode(node->right);
+            delete node;
+        }
+    }
+
 public:
     BSTree() : root(nullptr) {}
 
@@ -177,39 +191,159 @@ public:
         if (node) return node->index;
         return static_cast<size_t>(-1);  // return maximal size_t value as not found
     }
+
+    void Clear() {
+        ClearNode(root);
+        root = nullptr;
+    }
 };
+//help!!!!!
+void Heapify(vector<Pokemon> &pokemons, int n, int i) {
+    int largest = i;
+    int l = 2 * i + 1; // left = 2*i + 1
+    int r = 2 * i + 2; // right = 2*i + 2
+
+    if (l < n && pokemons[l].hp > pokemons[largest].hp)
+        largest = l;
+    
+    if (r < n && pokemons[r].hp > pokemons[largest].hp)
+        largest = r;
+    
+    if ((l < n && pokemons[l].hp == pokemons[largest].hp && l < largest) ||
+        (r < n && pokemons[r].hp == pokemons[largest].hp && r < largest)) {
+        largest = (l < r) ? l : r;
+    }
+    
+    if (largest != i) {
+        swap(pokemons[i], pokemons[largest]);
+        Heapify(pokemons, n, largest);
+    }
+}
+//建立最大heap 
+void BuildMaxHeap(vector<Pokemon> &pokemons) {
+    int n = pokemons.size();
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        Heapify(pokemons, n, i);
+    }
+}
+
+int HeapHeight(int n) {
+    return static_cast<int>(ceil(log2(n+1))); // n is the number of elements in the heap
+}
+// for 任務二 
+void Task2(vector<Pokemon> &pokemons) {
+	// build the max heap
+    BuildMaxHeap(pokemons);
+
+    // Output the max heap
+    cout << "        #       Name                            Type 1          HP      Attack  Defense" << "\n";
+    for (int i = 0; i < pokemons.size(); i++) {
+		if(i<9){
+			cout << "[ " << i+1 << "]\t";
+		}
+		else{
+			cout << "[" << i+1 << "]\t";
+		}
+		cout << pokemons[i].number << "\t";
+    	cout << pokemons[i].name << "\t\t\t";
+		if(pokemons[i].name.length() < 8){
+			cout << "\t";
+		}    	                
+    	cout << pokemons[i].type1 << "\t";
+		if(pokemons[i].type1.length() < 8){
+			cout << "\t";
+		}
+    	cout << pokemons[i].hp << "\t";
+    	cout << pokemons[i].attack << "\t";
+    	cout << pokemons[i].defense << endl;
+    }
+
+    // Calculate heap height
+    int height = HeapHeight(pokemons.size());
+
+    int leftmostIndex = (1 << (height - 1)) - 1;
+    if (leftmostIndex >= pokemons.size()) {
+        leftmostIndex = (1 << (height - 2)) - 1;
+    }
+    cout << "HP heap height = " << height << endl;
+
+    // Output leftmost node and bottom node
+    cout << "Leftmost node:\n[" << leftmostIndex << "]\t";
+    cout << pokemons[leftmostIndex].number << "\t";
+    cout << pokemons[leftmostIndex].name << "\t\t\t";
+	if(pokemons[leftmostIndex].name.length() < 8){
+		cout << "\t";
+	} 
+    cout << pokemons[leftmostIndex].type1 << "\t";
+	if(pokemons[leftmostIndex].type1.length() < 8){
+		cout << "\t";
+	} 
+    cout << pokemons[leftmostIndex].hp << "\t";
+    cout << pokemons[leftmostIndex].attack << "\t";
+    cout << pokemons[leftmostIndex].defense << endl;
+
+    cout << "Bottom:\n[" << pokemons.size() - 1 << "]\t";
+    
+    const Pokemon &bottom = pokemons.back();
+    cout << bottom.number << "\t";
+    cout << bottom.name << "\t\t\t";
+	if(bottom.name.length() < 8){
+		cout << "\t";
+	} 
+    cout << bottom.type1 << "\t";
+	if(bottom.type1.length() < 8){
+		cout << "\t";
+	} 
+    cout << bottom.hp << "\t";
+    cout << bottom.attack << "\t";
+    cout << bottom.defense << endl;   
+	 
+}
 
 int main() {
     BSTree tree;
     ifstream inputFile;
     string filenum;
-
+    vector<Pokemon> pokemons;
+	bool done = false;
     int command;
+
     do {
-    	GetCommand(command);
+        GetCommand(command);
+        switch (command) {
+            case 0: // Quit
 
-    	switch (command) {
-        	case 0: // Quit
-        	    cout << "Program terminates." << endl;
-        	    break;
-        	case 1: { // Read a file to build BST
-            	bool check;
-            	Inputtxt(inputFile, filenum, check);
-            	if (check) {
-                	vector<Pokemon> pokemons;
-                	string line;
+                break;
+            case 1:
+                bool check;
+                pokemons.clear();
+                tree.Clear();
+                Inputtxt(inputFile, filenum, check);
+                if (check) {
+                    string line;
 
-                	// Skip the header line
-                	getline(inputFile, line);
-	
-                	while (getline(inputFile, line)) {
-                	    pokemons.push_back(Pokemon::Parse(line));
-                	}
+                    // Skip the header line
+                    getline(inputFile, line);
+            		if(inputFile.eof() || check == false){ // 如果txt內沒東西 就結束 
+            			if(inputFile.eof()){
+            				cout << "\n### Get nothing from the file input" << filenum << ".txt ! ###\n" ;
+            				inputFile.close();
+            				done = false;
+							break;	
+						}
+					}
 
-                	// Displaying header
-                	cout << "        #       Name                            Type 1          HP      Attack  Defense" << endl;
+                    while (getline(inputFile, line)) {
+                        pokemons.push_back(Pokemon::Parse(line));
+                    }
+                	
 
-                	// Displaying pokemons
+                    inputFile.close();
+
+                    // Displaying header
+                    cout << "    " << "# Name Type 1 HP Attack Defense" << endl;
+
+                    // Displaying pokemons
                 	for (size_t i = 0; i < pokemons.size(); ++i) {
 						if(i<9){
 							cout << "[ " << i+1 << "]\t";
@@ -234,10 +368,10 @@ int main() {
     	                tree.Insert(pokemons[i], i);
     	            }
 
-    	            // Display the result
-    	            cout << "HP tree height = " << tree.TreeHeight() << endl;
-    	            Pokemon leftmost = tree.LeftmostNode();
-    	            size_t leftIndex = tree.LeftmostNodeIndex();
+                    // Display the result
+                    cout << "HP tree height = " << tree.TreeHeight() << endl;
+                    Pokemon leftmost = tree.LeftmostNode();
+                    size_t leftIndex = tree.LeftmostNodeIndex();
     	            cout << "Leftmost node:\n";
     	            cout << "        #       Name                            Type 1          HP      Attack  Defense" << "\n";
 
@@ -284,12 +418,19 @@ int main() {
     	            cout << rightmost.hp << "\t";
     	            cout << rightmost.attack << "\t";
     	            cout << rightmost.defense << endl;
+                }
+                done = true;
+                break;
+            case 2:
+            	if(done == true){
+            		Task2(pokemons);
+				}
+				else{
+					cout << endl << "----- Execute Mission 1 first! -----" << endl;
+				}
+                break;
+        }
+    } while (command != 0);
 
-    	        }
-    	        break;
-    	    }
-    	}
-
-	} while(command != 0) ;
     return 0;
 }
